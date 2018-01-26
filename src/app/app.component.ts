6180d788d29669da1d4e11fee8d70e46d93248e5
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2018-01-26 22:16:54 +0200

    initial implementation i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  @ViewChild('langSelect')langSelect: any;
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  useLanguage(language: string) {
+    console.log(language);
+    this.translate.use(language);
+  }
 }
